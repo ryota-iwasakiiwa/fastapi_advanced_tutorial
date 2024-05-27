c235f2faa8b73f4a7d7d4d108f88324c86852f62
ryota-iwasakiiwa <r.iwa411@gmail.com>
2024-05-27 10:07:09 +0900

    response cookies

---
+++ main_response_cookies.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.post("/cookie-and-object/")
+def create_cookie(response: Response):
+    response.set_cookie(key="fakesession", value="fake-cookie-session-value")
+    return {"message": "Come to the dark side, we have cookies"}
+
+@app.post("/cookie/")
+def create_cookie():
+    content = {"message": "Come to the dark side, we have cookies"}
+    response = JSONResponse(content=content)
+    response.set_cookie(key="fakesession", value="fake-cookie-session-value")
+    return response
