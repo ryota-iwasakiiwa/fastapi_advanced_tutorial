7d1b0baaad972a3ed8bd3c40788bf89c41d8c076
ryota-iwasakiiwa <r.iwa411@gmail.com>
2024-05-29 01:01:51 +0900

    including wsgi

---
+++ main_including_wsgi.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from flask import Flask, request
+from markupsafe import escape
+
+flask_app = Flask(__name__)
+
+@flask_app.route("/")
+def flask_main():
+    name = request.args.get("name", "Word")
+    return f"Hello, {escape(name)} from Flask!"
+
+app = FastAPI()
+
+@app.get("/v2")
+def read_root():
+    return {"message": "Hello World"}
+
+app.mount("/v1", WSGIMiddleware(flask_app))
